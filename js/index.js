5f3ca1d8197117810a5da7aa91171ebb648eec24
wybiral <davy.wybiral@gmail.com>
2016-09-20 08:51:02 -0500

    Clean up github API usage

--- js/index.js
+++ js/index.js
@@ -1,33 +1,57 @@
-window.onload = () => {
-    let api = 'https://api.github.com';
-    let url = api + '/users/wybiral/repos?sort=created';
-    fetch(url).then(response => {
+const htmlEscape = str => {
+    return ([
+        str,
+        [/&/g, '&amp;'],
+        [/>/g, '&gt;'],
+        [/</g, '&lt;'],
+        [/"/g, '&quot;'],
+        [/'/g, '&#39;'],
+        [/`/g, '&#96;']
+    ]).reduce((x, y) => x.replace(y[0], y[1]));
+};
+
+const getProjects = user => {
+    const api = 'https://api.github.com';
+    const url = api + '/users/' + user + '/repos';
+    const params = '?sort=created';
+    return fetch(url + params).then(response => {
         return response.json();
     }).then(results => {
-        let repos = document.querySelector('#repos');
+        const out = [];
         for (let x of results) {
-            if (!x.description) {
+            if (!x.description || x.fork) {
                 continue;
             }
-            let li = document.createElement('li');
-            let h2 = document.createElement('h2');
-            let a = document.createElement('a');
-            a.setAttribute('href', x.html_url);
-            let name = x.name;
-            let start = x.created_at.slice(0, 4);
-            name += ' (' + start;
-            let updated = x.pushed_at.slice(0, 4);
-            if (start !== updated) {
-                name += '-' + updated;
-            }
-            name += ')';
-            a.appendChild(document.createTextNode(name));
-            h2.appendChild(a);
-            li.appendChild(h2);
-            let div = document.createElement('div');
-            div.appendChild(document.createTextNode(x.description));
-            li.appendChild(div);
-            repos.appendChild(li);
+            out.push(x);
         }
+        return out;
+    });
+};
+
+const getDate = project => {
+    const created = project.created_at.slice(0, 4);
+    const pushed = project.pushed_at.slice(0, 4);
+    if (created === pushed) {
+        return created;
+    } else {
+        return created + '-' + pushed;
+    }
+};
+
+window.onload = () => {
+    let repos = document.querySelector('#repos');
+    getProjects('wybiral').then(projects => {
+        projects.forEach(project => {
+            const url = htmlEscape(project.html_url);
+            const name = htmlEscape(project.name);
+            const date = htmlEscape(getDate(project));
+            const description = htmlEscape(project.description);
+            repos.innerHTML += `
+                <li>
+                    <h2><a href="${url}">${name} (${date})</a></h2>
+                    <div>${description}</div>
+                </li>
+            `;
+        });
     });
 };
