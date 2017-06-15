34e5e36597b1c8e6e9a7a55ed61cca7396f3e5dd
wybiral <davy.wybiral@gmail.com>
2017-06-15 08:21:05 -0500

    Inline icons and build code page instead of using JS

---
+++ code/build.py
@@ -0,0 +1,24 @@
+from html import escape
+import requests
+
+def parse_date(proj):
+    created = escape(proj['created_at'][:4])
+    pushed = escape(proj['pushed_at'][:4])
+    if created == pushed:
+        return created
+    return created + '-' + pushed
+
+url = 'https://api.github.com/users/wybiral/repos?sort=created'
+item = '<li><h2><a href="{url}">{name} ({date})</a></h2><div>{desc}</div></li>'
+template = open('index.template.html', 'r').read()
+output = open('index.html', 'w')
+projects = ''
+for x in requests.get(url).json():
+    if not x['fork'] and x['description']:
+        url = escape(x['html_url'])
+        name = escape(x['name'])
+        date = parse_date(x)
+        desc = escape(x['description'])
+        projects += item.format(url=url, name=name, date=date, desc=desc)
+output.write(template.format(projects=projects))
+output.close()
